c151da01982312e7d8e9f18ac41340619e4e52b9
[lsqin] <[909366413@]>
2018-05-30 16:31:37 +0800

    23

--- app/Tester.py
+++ app/Tester.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import HttpResponse
+from app.models import host
 
 def Tester(request):
-    return "helloworld"
+    #查询
+    selectValue = host.objects.get(id = 1).hostName.encode("utf-8")
+    #修改
+    host.objects.filter(id = 1).update(hostName = "我的服务器")
+    #增加
+    host.objects.create(hostName = "我的服务器咯",hostUrl = "www.baidu.com")
+    #删除
+    host.objects.filter(id = 2).delete()
+
+    return HttpResponse(selectValue)
