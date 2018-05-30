179029f15dab57df81d0f3eca7fce44d324a8383
[lsqin] <[909366413@]>
2018-05-30 15:31:51 +0800

    222

--- app/Tester.py
+++ app/Tester.py
@@ -2,11 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import HttpResponse
 
-def createUser(request):
-    return HttpResponse("请求成功")
-
-def createHost(request):
-    return HttpResponse("请求成功")
-
-def createUrl(request):
-    return HttpResponse("请求成功")
+def Tester(request):
+    return "helloworld"
