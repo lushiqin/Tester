4449c3246d4e095fa7ad12651f031a95ea0d2e86
[lsqin] <[909366413@]>
2018-07-02 18:11:19 +0800

    定时任务

--- app/controller/fromId.py
+++ app/controller/fromId.py
@@ -2,6 +2,9 @@ from django.shortcuts import HttpResponse,render
 from app import models
 import simplejson
 import time
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import register_job,register_events,DjangoJobStore
 
 def addFromId(request):
     responseBody = request.body
@@ -23,3 +26,16 @@ def addFromId(request):
 def secOne(openId):
     result = models.userFromId.objects.filter(status=1, openId=openId ).values()[0]
     return result
+
+schedulers = BackgroundScheduler()
+@register_job(schedulers,"cron",day_of_week ='0-6',hour='00',minute='00',second='00',id="fromid_job")
+def updFromid():
+    print("跑定时任务updFromid",time.time())
+    time7 = datetime.datetime.now()-datetime.timedelta(days=-6)
+    inttime = int(time.mktime(time7.timetuple()))
+    models.userFromId.objects.filter(create_time__gte=inttime).update(status =0,update_time=time.time())
+try:
+    schedulers.start()
+except (KeyboardInterrupt,SystemExit):
+    print("shutdown")
+    schedulers.shutdown()
